c0235b754af5f98079f2c8a6d7a0a050160f8225
myf2ar10 <myf2ar10@gmail.com>
2023-11-08 16:15:06 +0000

    [add]mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,12 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  # default from: 'from@example.com'
+  default from: 'amulabamulab@gmail.com'
   layout 'mailer'
+  
+  def welcome_email
+    @admin.email = amulabamulab@gmail.com
+    @user = params[:user]
+    # mail(to: @user.email, subject: 'メールのタイトルがここに入ります')
+    mail(to: @admin.email, subject: '【Amlab管理者宛】Amlabにてお問い合わせがありました。')
+  end
 end
