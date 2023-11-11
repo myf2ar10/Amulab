590a9035c7ca1183a7933393b85d39cda667f5f0
myf2ar10 <myf2ar10@gmail.com>
2023-11-11 13:41:36 +0000

    [update]mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,12 +1,12 @@
 class ApplicationMailer < ActionMailer::Base
-  # default from: 'from@example.com'
+
   default from: 'amulabamulab@gmail.com'
   layout 'mailer'
 
   def welcome_email
     @admin.email = 'amulabamulab@gmail.com'
     @user = params[:user]
-    # mail(to: @user.email, subject: 'メールのタイトルがここに入ります')
-    mail(to: @admin.email, subject: '【Amlab管理者宛】Amlabにてお問い合わせがありました。')
+    mail(to: @user.email, subject: '【Amulab閲覧者宛】Amulab welcome_email')
+    mail(to: @admin.email, subject: '【Amulab管理者宛】Amulab_welcome_email')
   end
 end
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,26 +1,8 @@
 class ContactMailer < ApplicationMailer
-  # def send_mail(contact)
-  #   @contact = contact
-    # mail to:   ENV['TOMAIL'], subject: '【お問い合わせ】' + @contact.subject_i18n
-  # end
-  # def send_mail(contact)
-  #   @contact = contact
-  #   # subject_prefix = '&#8203;``【oaicite:0】``&#8203;' # 件名のプレフィックス
-  #   # subject = subject_prefix + @contact.subject.to_s # 整数を文字列に変換
-  #   mail to: ENV['TOMAIL'],
-  #   # subject: subject
-  #   mail to:   ENV['TOMAIL'], subject: '【お問い合わせ】'
-  #   # + @contact.subject_i18n
-  #   mail to: ENV['TOMAIL'], subject: "【Amulabお問い合わせ】"+ @contact.name+"さんより"
-  # end
-
-    def send_mail(contact)
-    @contact = contact
-    mail(to: ENV['TOMAIL'], subject: "【Amulabお問い合わせ】"+ @contact.name+"さんより")
-    end
 
   def send_mail(contact)
-    @contact = contact
-    mail(to: 'amulabamulab@gmail.com', subject: "【Amulabお問い合わせ】"+ @contact.name+"さんより")
+  @contact = contact
+  mail(to: ENV['TOMAIL'], subject: "【Amulabお問い合わせ】"+ @contact.name+"さんより")
   end
+
 end
