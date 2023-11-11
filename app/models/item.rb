9f83a20d0db748083b4f8c8cfc0c49d0af635acb
myf2ar10 <myf2ar10@gmail.com>
2023-11-11 08:20:46 +0000

    [update]mailer

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,6 +8,9 @@ class Item < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :item_comments, dependent: :destroy
 
+  validates :name, presence: true
+  validates :description, presence: true
+
   def favorited?(user)
     favorites.where(user_id: user.id).exists?
   end
