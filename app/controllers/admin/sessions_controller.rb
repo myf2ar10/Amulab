2127212b89b59018ef8e8d0c1e76bbd5f4612665
myf2ar10 <myf2ar10@gmail.com>
2023-11-15 16:24:43 +0000

    [update]search

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -41,7 +41,7 @@ class Admin::SessionsController < Devise::SessionsController
   protected
 
   def after_sign_in_path_for(resource)
-    admin_admin_top_path
+    public_root_path
   end
 
   # def after_sign_out_path_for(resource)
