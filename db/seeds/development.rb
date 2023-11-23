f6e66056fe1d63563e5447621d505c6d88f695ea
myf2ar10 <myf2ar10@gmail.com>
2023-11-23 14:14:24 +0000

    [update]seed

---
+++ db/seeds/development.rb
@@ -0,0 +1,250 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+Admin.create!(
+id: 1,
+  email: "admin@admin",
+  password: "123456"
+  )
+
+User.create!(
+id: 0,
+  email: "user@user.com",
+  name: "ユーザー",
+  phone_number: "0000000000",
+  password: "123456"
+  )
+
+User.create!(
+id: 2,
+  email: "user2@user2.com",
+  name: "ユーザー2",
+  phone_number: "0222222222",
+  password: "123456"
+  )
+
+User.create!(
+id: 3,
+  email: "user3@user3.com",
+  name: "ユーザー3",
+  phone_number: "0333333333",
+  password: "123456"
+  )
+
+10.times do |n|
+  User.create!(
+      email: Faker::Internet.email,
+      name: Faker::Japanese::Name.name,
+      phone_number: "012345678#{n + 1}",
+      password: "123456",
+    )
+end
+
+Genre.create!(
+id: 1,
+  name: "テスト_ジャンル"
+  )
+
+Genre.create!(
+id: 2,
+  name: "テスト_ジャンル2"
+  )
+
+Genre.create!(
+id: 3,
+  name: "テスト_ジャンル3"
+  )
+
+Genre.create!(
+id: 4,
+  name: "テスト_ジャンル4"
+  )
+
+Genre.create!(
+id: 5,
+  name: "テスト_ジャンル5"
+  )
+
+Genre.create!(
+id: 6,
+  name: "テスト_ジャンル6"
+  )
+
+Genre.create!(
+id: 7,
+  name: "テスト_ジャンル7"
+  )
+
+Genre.create!(
+id: 8,
+  name: "テスト_ジャンル8"
+  )
+
+Genre.create!(
+id: 9,
+  name: "テスト_ジャンル9"
+  )
+
+Genre.create!(
+id: 10,
+  name: "テスト_ジャンル10"
+  )
+
+10.times do |n|
+  id = 10 + n + 1
+  Genre.create!(
+      id: id,
+      name: Faker::Games::SuperMario.character + "ジャンル"
+      # Faker::Music.genre
+      # Faker::ProgrammingLanguage.name,
+    )
+end
+
+Item.create!(
+id: 1,
+  user: User.find_by(name: "ユーザー"),
+  genre: Genre.find_by(name: "テスト_ジャンル"),
+  name: "テスト_作品",
+  description: "テスト_作品説明文"
+  )
+item = Item.find_by(name: "テスト_作品")
+ item.images.attach(io: File.open(Rails.root.join('app/assets/images/test.jpg')), filename: 'test_item')
+
+Item.create!(
+id: 2,
+  user: User.find_by(name: "ユーザー"),
+  genre: Genre.find_by(name: "テスト_ジャンル2"),
+  name: "テスト_作品2",
+  description: "テスト_作品説明文2"
+  )
+item = Item.find_by(name: "テスト_作品2")
+ item.images.attach(io: File.open(Rails.root.join('app/assets/images/test2.png')), filename: 'test_item')
+item = Item.find_by(name: "テスト_作品2")
+ item.images.attach(io: File.open(Rails.root.join('app/assets/images/test2_2.png')), filename: 'test_item')
+
+Item.create!(
+id: 3,
+  user: User.find_by(name: "ユーザー"),
+  genre: Genre.find_by(name: "テスト_ジャンル3"),
+  name: "テスト_作品3",
+  description: "テスト_作品説明文3"
+  )
+# item = Item.find_by(name: "テスト_作品3")
+# item.images.attach(io: File.open(Rails.root.join('app/assets/images/test.jpg')), filename: 'test_item')
+
+Item.create!(
+id: 4,
+  user: User.find_by(name: "ユーザー"),
+  genre: Genre.find_by(name: "テスト_ジャンル4"),
+  name: "テスト_作品4",
+  description: "テスト_作品説明文4"
+  )
+item = Item.find_by(name: "テスト_作品4")
+ item.images.attach(io: File.open(Rails.root.join('app/assets/images/test4.png')), filename: 'test_item')
+item = Item.find_by(name: "テスト_作品4")
+ item.images.attach(io: File.open(Rails.root.join('app/assets/images/test4.png')), filename: 'test_item')
+item = Item.find_by(name: "テスト_作品4")
+ item.images.attach(io: File.open(Rails.root.join('app/assets/images/test4.png')), filename: 'test_item')
+
+Item.create!(
+id: 5,
+  user: User.find_by(name: "ユーザー"),
+  genre: Genre.find_by(name: "テスト_ジャンル5"),
+  name: "テスト_作品5",
+  description: "テスト_作品説明文5"
+  )
+item = Item.find_by(name: "テスト_作品5")
+ item.images.attach(io: File.open(Rails.root.join('app/assets/images/test5.png')), filename: 'test_item')
+
+Item.create!(
+id: 6,
+  user: User.find_by(name: "ユーザー"),
+  genre: Genre.find_by(name: "テスト_ジャンル6"),
+  name: "テスト_作品6",
+  description: "テスト_作品説明文6"
+  )
+item = Item.find_by(name: "テスト_作品6")
+ item.images.attach(io: File.open(Rails.root.join('app/assets/images/test6.png')), filename: 'test_item')
+
+Item.create!(
+id: 7,
+  user: User.find_by(name: "ユーザー"),
+  genre: Genre.find_by(name: "テスト_ジャンル7"),
+  name: "テスト_作品7",
+  description: "テスト_作品説明文7"
+  )
+item = Item.find_by(name: "テスト_作品7")
+ item.images.attach(io: File.open(Rails.root.join('app/assets/images/test7.png')), filename: 'test_item')
+
+Item.create!(
+id: 8,
+  user: User.find_by(name: "ユーザー"),
+  genre: Genre.find_by(name: "テスト_ジャンル8"),
+  name: "テスト_作品8",
+  description: "テスト_作品説明文8"
+  )
+item = Item.find_by(name: "テスト_作品8")
+ item.images.attach(io: File.open(Rails.root.join('app/assets/images/test8.png')), filename: 'test_item')
+item = Item.find_by(name: "テスト_作品8")
+ item.images.attach(io: File.open(Rails.root.join('app/assets/images/test8_2.png')), filename: 'test_item')
+
+Item.create!(
+id: 9,
+  user: User.find_by(name: "ユーザー"),
+  genre: Genre.find_by(name: "テスト_ジャンル9"),
+  name: "テスト_作品9",
+  description: "テスト_作品説明文9"
+  )
+item = Item.find_by(name: "テスト_作品9")
+ item.images.attach(io: File.open(Rails.root.join('app/assets/images/test9.png')), filename: 'test_item')
+
+Item.create!(
+id: 10,
+  user: User.find_by(name: "ユーザー"),
+  genre: Genre.find_by(name: "テスト_ジャンル10"),
+  name: "テスト_作品10",
+  description: "テスト_作品説明文10"
+  )
+item = Item.find_by(name: "テスト_作品10")
+ item.images.attach(io: File.open(Rails.root.join('app/assets/images/test10.png')), filename: 'test_item')
+item = Item.find_by(name: "テスト_作品10")
+ item.images.attach(io: File.open(Rails.root.join('app/assets/images/test10_2.png')), filename: 'test_item')
+
+5.times do |n|
+    id = 10 + n + 1
+  Item.create!(
+      id: id,
+      user: User.find_by(name: "ユーザー2"),
+      genre: Genre.find_by(id: 10 + n + 1),
+      name: Faker::Games::Pokemon.name + "って作品",
+      description: Faker::Games::Pokemon.location + "みたいな作品です。ああああああああああああああああああああああああああああああ。",
+    )
+end
+
+5.times do |n|
+item = Item.find_by(id: 10 + n + 1)
+item.images.attach(io: File.open(Rails.root.join('app/assets/images/test_11-15.png')), filename: 'test_item')
+end
+
+5.times do |n|
+    id = 15 + n + 1
+  Item.create!(
+      id: id,
+      user: User.find_by(name: "ユーザー3"),
+      genre: Genre.find_by(id: 15 + n + 1),
+      name: Faker::Games::Pokemon.name + "って作品",
+      description: Faker::Games::Pokemon.location + "みたいな作品です。いいいいいいいいいいいいいいいいいいいいいいいいいいいいいい。",
+    )
+end
+
+5.times do |n|
+item = Item.find_by(id: 15 + n + 1)
+item.images.attach(io: File.open(Rails.root.join('app/assets/images/test_16-20.png')), filename: 'test_item')
+end
---
+++ db/seeds/production.rb
@@ -0,0 +1,198 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.create!(
+id: 1,
+  email: "admin@admin",
+  password: "123456"
+  )
+
+User.create!(
+id: 0,
+  email: "user@user.com",
+  name: "編み物大好きっ子",
+  phone_number: "0000000000",
+  password: "123456"
+  )
+
+User.create!(
+id: 2,
+  email: "user2@user2.com",
+  name: "はなちゃん",
+  phone_number: "0222222222",
+  password: "123456"
+  )
+
+User.create!(
+id: 3,
+  email: "user3@user3.com",
+  name: "真子",
+  phone_number: "0333333333",
+  password: "123456"
+  )
+
+
+Genre.create!(
+id: 1,
+  name: "いいねが欲しい"
+  )
+
+Genre.create!(
+id: 2,
+  name: "コメントが欲しい"
+  )
+
+Genre.create!(
+id: 3,
+  name: "自分用のメモ"
+  )
+
+Genre.create!(
+id: 4,
+  name: "その他"
+  )
+
+Genre.create!(
+id: 5,
+  name: "かばん"
+  )
+
+Genre.create!(
+id: 6,
+  name: "ポーチ"
+  )
+
+Genre.create!(
+id: 7,
+  name: "編み物"
+  )
+
+Genre.create!(
+id: 8,
+  name: "縫い物"
+  )
+
+Genre.create!(
+id: 9,
+  name: "適当に作ってみた"
+  )
+
+Genre.create!(
+id: 10,
+  name: "お気に入り"
+  )
+
+Item.create!(
+id: 1,
+  user: User.find_by(name: "編み物大好きっ子"),
+  genre: Genre.find_by(name: "適当に作ってみた"),
+  name: "リトルグリーンメンのミニバッグ",
+  description: "ビニールひもで編みました。試しに作ったので網目がずれています。楽しく作れました！"
+  )
+item = Item.find_by(name: "リトルグリーンメンのミニバッグ")
+ item.images.attach(io: File.open(Rails.root.join('app/assets/images/test.jpg')), filename: 'test_item')
+
+Item.create!(
+id: 2,
+  user: User.find_by(name: "編み物大好きっ子"),
+  genre: Genre.find_by(name: "かばん"),
+  name: "ミニバッグ",
+  description: "100均で買った毛糸で編みました。どこの100均か忘れました💦バイカラーにしてこだわっています。これを作ったときは初めて底板を使いました。持ち手も既製品を使ったので丈夫で、今も使っているバッグです。"
+  )
+item = Item.find_by(name: "ミニバッグ")
+ item.images.attach(io: File.open(Rails.root.join('app/assets/images/test2.png')), filename: 'test_item')
+item = Item.find_by(name: "ミニバッグ")
+ item.images.attach(io: File.open(Rails.root.join('app/assets/images/test2_2.png')), filename: 'test_item')
+
+Item.create!(
+id: 3,
+  user: User.find_by(name: "編み物大好きっ子"),
+  genre: Genre.find_by(name: "自分用のメモ"),
+  name: "メモ",
+  description: "次回作ったらここを編集。画像未貼付"
+  )
+# item = Item.find_by(name: "メモ")
+# item.images.attach(io: File.open(Rails.root.join('app/assets/images/test.jpg')), filename: 'test_item')
+
+Item.create!(
+id: 4,
+  user: User.find_by(name: "はなちゃん"),
+  genre: Genre.find_by(name: "編み物"),
+  name: "ミニオン",
+  description: "余っていたビニールひもを使って編み図なしで編んだ。ミニオン。手が痛かった。"
+  )
+item = Item.find_by(name: "ミニオン")
+ item.images.attach(io: File.open(Rails.root.join('app/assets/images/test4.png')), filename: 'test_item')
+item = Item.find_by(name: "ミニオン")
+ item.images.attach(io: File.open(Rails.root.join('app/assets/images/test4.png')), filename: 'test_item')
+item = Item.find_by(name: "ミニオン")
+ item.images.attach(io: File.open(Rails.root.join('app/assets/images/test4.png')), filename: 'test_item')
+
+Item.create!(
+id: 5,
+  user: User.find_by(name: "はなちゃん"),
+  genre: Genre.find_by(name: "編み物"),
+  name: "バッグ",
+  description: "ベロア風のバッグを編んだ。チャームを余りもの。糸処理が雑になったので次回気を付ける。"
+  )
+item = Item.find_by(name: "バッグ")
+ item.images.attach(io: File.open(Rails.root.join('app/assets/images/test5.png')), filename: 'test_item')
+
+Item.create!(
+id: 6,
+  user: User.find_by(name: "はなちゃん"),
+  genre: Genre.find_by(name: "その他"),
+  name: "座布団",
+  description: "ネットの編み図を見ながら座布団を編んだ。極太の意図を使った。いろんな色のモチーフをたくさん作るのが楽しかった。"
+  )
+item = Item.find_by(name: "座布団")
+ item.images.attach(io: File.open(Rails.root.join('app/assets/images/test6.png')), filename: 'test_item')
+
+Item.create!(
+id: 7,
+  user: User.find_by(name: "真子"),
+  genre: Genre.find_by(name: "その他"),
+  name: "貯金箱",
+  description: "昔作った工作の貯金箱です。出来が良かったので記録として登録しておきます。確か表彰されました。嬉しかったです。"
+  )
+item = Item.find_by(name: "貯金箱")
+ item.images.attach(io: File.open(Rails.root.join('app/assets/images/test7.png')), filename: 'test_item')
+
+Item.create!(
+id: 8,
+  user: User.find_by(name: "真子"),
+  genre: Genre.find_by(name: "お気に入り"),
+  name: "ピンクのかばん",
+  description: "衣類を一時的に仕舞う用に入れ物が欲しかったので適当に編んでみました。結構気に入っています。"
+  )
+item = Item.find_by(name: "ピンクのかばん")
+ item.images.attach(io: File.open(Rails.root.join('app/assets/images/test8.png')), filename: 'test_item')
+item = Item.find_by(name: "ピンクのかばん")
+ item.images.attach(io: File.open(Rails.root.join('app/assets/images/test8_2.png')), filename: 'test_item')
+
+Item.create!(
+id: 9,
+  user: User.find_by(name: "真子"),
+  genre: Genre.find_by(name: "縫い物"),
+  name: "携帯ケース",
+  description: "ポケットのない服を着ているときに携帯を入れておくところがないのでケースを縫いました。便利です★"
+  )
+item = Item.find_by(name: "携帯ケース")
+ item.images.attach(io: File.open(Rails.root.join('app/assets/images/test9.png')), filename: 'test_item')
+
+Item.create!(
+id: 10,
+  user: User.find_by(name: "真子"),
+  genre: Genre.find_by(name: "ポーチ"),
+  name: "ポーチ",
+  description: "何か小物を入れる用のポーチです。"
+  )
+item = Item.find_by(name: "ポーチ")
+ item.images.attach(io: File.open(Rails.root.join('app/assets/images/test10.png')), filename: 'test_item')
+item = Item.find_by(name: "ポーチ")
+ item.images.attach(io: File.open(Rails.root.join('app/assets/images/test10_2.png')), filename: 'test_item')
