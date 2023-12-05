c778562374edcabf872795ca5648eaa66325f4cc
myf2ar10 <myf2ar10@gmail.com>
2023-12-05 02:28:32 +0000

    [update]API

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_12_03_115758) do
+ActiveRecord::Schema.define(version: 2023_12_05_014515) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -100,6 +100,7 @@ ActiveRecord::Schema.define(version: 2023_12_03_115758) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.json "images"
+    t.decimal "score", precision: 5, scale: 3
   end
 
   create_table "searches", force: :cascade do |t|
